18a4709d7adbe121d6cda0f6b184ff72f2fafa31
Oey Darryl Valencio Wijaya <Darryl16888@gmail.com>
2021-11-08 10:10:59 +0700

    First commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,7 +6,7 @@
 <div class="container text-dark text-center">
   <h1 class="neonText">Contact</h1>
     <div class="card rounded mb-5" style="width:400px; margin:auto;">
-        <img class="card-img-top img-fluid img-thumbnail img-responsive rounded" src="http://127.0.0.1:8000/darryl.jpg" alt="Card image">
+        <img class="card-img-top img-fluid img-thumbnail img-responsive rounded" src="{{ asset('darryl.jpg') }}" alt="Card image">
         <div class="card-body">
         <h4 class="card-title">Oey Darryl Valencio Wijaya</h4>
         <a href="https://discord.com/users/203113049698074624" style="color: rgb(24, 49, 83);"><i class="fa-brands fa-discord fa-fade fa-2xl mb-3"></i></a>
